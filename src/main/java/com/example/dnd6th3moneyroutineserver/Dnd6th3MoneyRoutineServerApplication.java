acf19e50adc33de9a5ef0af7760775eca21254ea
rlacksgus97 <rlacksgus97@naver.com>
2022-01-12 19:24:30 +0900

    feat: springboot, mysql setting

---
+++ src/main/java/com/example/dnd6th3moneyroutineserver/Dnd6th3MoneyRoutineServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnd6th3moneyroutineserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd6th3MoneyRoutineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd6th3MoneyRoutineServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dnd6th3moneyroutineserver/Dnd6th3MoneyRoutineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnd6th3moneyroutineserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd6th3MoneyRoutineServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
