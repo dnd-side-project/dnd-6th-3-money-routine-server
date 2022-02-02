fd3fd595e4ec86df36ed19391c67ca564778c566
onAuspicious <wotj7687@icloud.com>
2022-02-02 17:48:07 +0900

    feat: Add category repository

---
+++ src/main/java/com/example/dnd6th3moneyroutineserver/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.dnd6th3moneyroutineserver.repository;
+
+import com.example.dnd6th3moneyroutineserver.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
