3cfa64b9e92e4e91deae99f61aa565a126c1053f
onAuspicious <wotj7687@icloud.com>
2022-02-02 18:02:00 +0900

    feat: Add repositories

---
+++ src/main/java/com/example/dnd6th3moneyroutineserver/repository/ExpenditureRepository.java
@@ -0,0 +1,8 @@
+package com.example.dnd6th3moneyroutineserver.repository;
+
+import com.example.dnd6th3moneyroutineserver.entity.Expenditure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenditureRepository extends JpaRepository<Expenditure, Long> {
+
+}
