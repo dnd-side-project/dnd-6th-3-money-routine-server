7cb8995cbf8ad1dec43f8fa012f2b4116bc308f0
onAuspicious <wotj7687@icloud.com>
2022-02-09 18:07:00 +0900

    fix: Update return type ResponseEntity

--- src/main/java/com/example/dnd6th3moneyroutineserver/customCategory/CustomCategoryController.java
+++ src/main/java/com/example/dnd6th3moneyroutineserver/customCategory/CustomCategoryController.java
@@ -1,8 +1,14 @@
 package com.example.dnd6th3moneyroutineserver.customCategory;
 
+import com.example.dnd6th3moneyroutineserver.common.CustomResponse;
+import com.example.dnd6th3moneyroutineserver.common.ResponseMessage;
+import com.example.dnd6th3moneyroutineserver.common.StatusCode;
 import com.example.dnd6th3moneyroutineserver.customCategory.dto.CustomCategoryCreateDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +26,9 @@ public class CustomCategoryController {
      * Create new custom category
      */
     @PostMapping
-    public Long create(@RequestBody CustomCategoryCreateDto customCategoryCreateDto) {
-        return customCategoryService.add(customCategoryCreateDto);
+    @ApiOperation(value = "사용자 카테고리 생성", notes = "사용자가 만들고싶은 추가 카테고리 생성")
+    public ResponseEntity create(@RequestBody CustomCategoryCreateDto customCategoryCreateDto) {
+        return new ResponseEntity(CustomResponse
+                .response(StatusCode.OK, ResponseMessage.CREATE_CUSTOM_CATEGORY_SUCCESS, customCategoryService.add(customCategoryCreateDto)), HttpStatus.OK);
     }
 }
