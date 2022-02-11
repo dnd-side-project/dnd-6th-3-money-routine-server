f34dd82c2564049ec55142f4465abc5442fc38e6
rlacksgus97 <rlacksgus97@naver.com>
2022-02-11 09:48:33 +0900

    fix: add lombok annotations

--- src/main/java/com/example/dnd6th3moneyroutineserver/category/Category.java
+++ src/main/java/com/example/dnd6th3moneyroutineserver/category/Category.java
@@ -1,13 +1,14 @@
 package com.example.dnd6th3moneyroutineserver.category;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Builder
-@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Category {
 
     @Id @GeneratedValue
