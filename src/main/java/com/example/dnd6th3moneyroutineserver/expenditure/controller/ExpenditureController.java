db73e7ba29832c46b4981d93ce53edadac339f95
rlacksgus97 <rlacksgus97@naver.com>
2022-02-11 21:00:20 +0900

    fix: split getStatistics API into two (weekly, monthly)

--- src/main/java/com/example/dnd6th3moneyroutineserver/expenditure/controller/ExpenditureController.java
+++ src/main/java/com/example/dnd6th3moneyroutineserver/expenditure/controller/ExpenditureController.java
@@ -26,24 +26,38 @@ public class ExpenditureController {
                 .response(StatusCode.OK, ResponseMessage.WRITE_SUCCESS, expenditureService.write(expenditureWriteDto)), HttpStatus.OK);
     }
 
-    @GetMapping("/statistics")
-    @ApiOperation(value = "기간 소비 내역 조회", notes = "기간동안 소비 내역을 조회한다. 원형 그래프 영역")
-    public ResponseEntity getExpenditureStatistics(StatisticsRequestDto statisticsRequestDto) {
+    @GetMapping("/statistics/weekly")
+    @ApiOperation(value = "주별 소비 내역 조회", notes = "가장 많이 지출한 분야명, 총 지출 금액, 분야별 지출 금액, 비율 및 지출 내역")
+    public ResponseEntity getWeeklyStatistics(StatisticsRequestDto statisticsRequestDto) {
         return new ResponseEntity(CustomResponse
-                .response(StatusCode.OK, ResponseMessage.STATISTICS_SUCCESS, expenditureService.getStatistics(statisticsRequestDto)), HttpStatus.OK);
+                .response(StatusCode.OK, ResponseMessage.WEEKLY_STATISTICS_SUCCESS, expenditureService.weeklyStatistics(statisticsRequestDto)), HttpStatus.OK);
+    }
+
+    @GetMapping("/statistics/monthly")
+    @ApiOperation(value = "월별 소비 내역 조회", notes = "가장 많이 지출한 분야명, 총 지출 금액, 분야별 지출 금액, 비율 (지출 내역은 제외)")
+    public ResponseEntity getMonthlyStatistics(StatisticsRequestDto statisticsRequestDto) {
+        return new ResponseEntity(CustomResponse
+                .response(StatusCode.OK, ResponseMessage.MONTHLY_STATISTICS_SUCCESS, expenditureService.monthlyStatistics(statisticsRequestDto)), HttpStatus.OK);
+    }
+
+    @GetMapping("/statistics/monthly/detail/{categoryId}/{isCustom}")
+    @ApiOperation(value = "월별 카테고리별 소비 내역 상세 조회", notes = "해당 월 해당 카테고리에 대한 소비 내역 상세 조회")
+    public ResponseEntity getMonthlyDetails(@PathVariable Long categoryId, @PathVariable boolean isCustom, StatisticsRequestDto statisticsRequestDto) {
+        return new ResponseEntity(CustomResponse
+                .response(StatusCode.OK, ResponseMessage.MONTHLY_STATISTICS_SUCCESS, expenditureService.monthlyDetails(categoryId, isCustom, statisticsRequestDto)), HttpStatus.OK);
     }
 
     @GetMapping("/weekly")
     @ApiOperation(value = "주별 소비 동향 조회", notes = "주별 소비 동향을 조회한다. 막대 그래프 영역")
     public ResponseEntity getWeeklyTendency() {
         return new ResponseEntity(CustomResponse
-                .response(StatusCode.OK, ResponseMessage.WEEKLY_SUCCESS, expenditureService.getWeekly()), HttpStatus.OK);
+                .response(StatusCode.OK, ResponseMessage.WEEKLY_TENDENCY_SUCCESS, expenditureService.getWeekly()), HttpStatus.OK);
     }
 
     @GetMapping("/monthly")
     @ApiOperation(value = "월별 소비 동향 조회", notes = "월별 소비 동향을 조회한다. 막대 그래프 영역")
     public ResponseEntity getMonthlyTendency() {
         return new ResponseEntity(CustomResponse
-                .response(StatusCode.OK, ResponseMessage.MONTHLY_SUCCESS, expenditureService.getMonthly()), HttpStatus.OK);
+                .response(StatusCode.OK, ResponseMessage.MONTHLY_TENDENCY_SUCCESS, expenditureService.getMonthly()), HttpStatus.OK);
     }
 }
