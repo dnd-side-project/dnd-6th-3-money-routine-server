49a9b6cd67c297bb43d5ef162433ad197f6b5914
rlacksgus97 <rlacksgus97@naver.com>
2022-02-14 14:43:48 +0900

    feat: add getMonthlyTendency API

--- src/main/java/com/example/dnd6th3moneyroutineserver/expenditure/dto/MonthExpenseInfoDto.java
+++ src/main/java/com/example/dnd6th3moneyroutineserver/expenditure/dto/MonthExpenseInfoDto.java
@@ -1,2 +1,14 @@
-package com.example.dnd6th3moneyroutineserver.expenditure.dto;public class MonthExpenseInfoDto {
+package com.example.dnd6th3moneyroutineserver.expenditure.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MonthExpenseInfoDto {
+    private int month;
+    private int budget;
+    private int monthExpense;
 }
--- src/main/java/com/example/dnd6th3moneyroutineserver/expenditure/dto/MonthlyTendencyResponseDto.java
+++ src/main/java/com/example/dnd6th3moneyroutineserver/expenditure/dto/MonthlyTendencyResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.dnd6th3moneyroutineserver.expenditure.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MonthlyTendencyResponseDto {
+    List<MonthExpenseInfoDto> monthExpenseInfoDtoList;
 }
